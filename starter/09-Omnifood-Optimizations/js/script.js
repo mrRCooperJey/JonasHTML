1f5cbac2ddf521a38b8231c7701997140fc159e0
mrRCooperJey <vityak007@gmail.com>
2023-06-19 21:07:49 +0300

    adaptive 0.3

--- starter/09-Omnifood-Optimizations/js/script.js
+++ starter/09-Omnifood-Optimizations/js/script.js
@@ -1,3 +1,31 @@
+// Меню гамбургер
+const btnNav = document.querySelector('.btn-mobile-nav')
+const headerEl = document.querySelector('.header')
+
+btnNav.addEventListener('click', () => {
+  headerEl.classList.toggle('nav-open')
+})
+
+// Плавная прокрутка
+const allLinks = document.querySelectorAll('a')
+allLinks.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault()
+    const href = link.getAttribute('href')
+
+    // Scroll back to top
+    if (href === '#') window.scrollTo({ top: 0, behavior: "smooth" })
+    // Scroll to other links
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href)
+      sectionEl.scrollIntoView({behavior:"smooth"})
+    }
+    // Close mobile nabigation
+    if (link.classList.contains('main-nav-link'))
+      headerEl.classList.toggle('nav-open')
+  })
+})
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
